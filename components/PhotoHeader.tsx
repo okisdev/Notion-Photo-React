fc318aad68ac0956c80267e9b9314b8e2d8143d1
Harry Yep <github@hode.uk>
2022-03-05 22:23:28 +0000

    add header description to config

--- components/PhotoHeader.tsx
+++ components/PhotoHeader.tsx
@@ -1,5 +1,11 @@
 import siteConfig from '../config/site.config';
 
+const stringToJSX = () => {
+    return {
+        __html: siteConfig.global.content.header.description,
+    };
+};
+
 const PhotoHeader = () => {
     return (
         <div id='notion-photo-header' className='flex flex-row justify-center items-center my-10 space-x-2 md:space-x-8 dark:text-[#adbac7]'>
@@ -9,45 +15,7 @@ const PhotoHeader = () => {
                 </div>
                 <div id='notion-photo-header-description' className='pt-6'>
                     <span className='text-center'>
-                        Photo Gallery built with{' '}
-                        <a href='https://notion.so/' className='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>
-                            Notion
-                        </a>
-                        ,{' '}
-                        <a href='https://reactjs.org/' className='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>
-                            React
-                        </a>
-                        ,{' '}
-                        <a href='https://nextjs.org/' className='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>
-                            Next.js
-                        </a>
-                        ,{' '}
-                        <a href='https://tailwindcss.com/' className='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>
-                            tailwindcss
-                        </a>
-                        ,{' '}
-                        <a href='https://www.typescriptlang.org/' className='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>
-                            TypeScript
-                        </a>
-                        ,{' '}
-                        <a
-                            href='https://github.com/splitbee/notion-api-worker'
-                            className='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        >
-                            Notion-Api-Worker
-                        </a>{' '}
-                        and more. (Find more on{' '}
-                        <a
-                            href='https://github.com/Harry-Yep/Notion-Photo-React'
-                            className='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        >
-                            GitHub
-                        </a>
-                        )
+                        <div dangerouslySetInnerHTML={stringToJSX()}></div>
                     </span>
                 </div>
             </div>
--- config/site.config.js
+++ config/site.config.js
@@ -8,6 +8,9 @@ module.exports = {
             banner_img: `https://cdn.harrly.com/project/GitHub/Notion-Photo-React/img/Notion-Photo-React.Banner.png`,
         },
         content: {
+            header: {
+                description: `Photo Gallery built with <a href='https://notion.so/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>Notion</a>, <a href='https://reactjs.org/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>React</a>, <a href='https://nextjs.org/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>Next.js</a>, <a href='https://tailwindcss.com/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>tailwindcss</a>, <a href='https://www.typescriptlang.org/' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>TypeScript</a>, <a href='https://github.com/splitbee/notion-api-worker' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>Notion-Api-Worker</a> and more. (Find more on <a href='https://github.com/Harry-Yep/Notion-Photo-React' class='transition duration-500 underline hover:bg-yellow-500 dark:hover:bg-yellow-600' target='_blank' rel='noopener noreferrer'>GitHub</a>)`,
+            },
             license: {
                 name: 'CC BY-NC-SA 4.0',
                 url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
