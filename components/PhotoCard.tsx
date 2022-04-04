6e497dce1c4260fd619815a78ba8df7431571124
Harry Yep <github@hode.uk>
2022-04-04 18:08:18 +0100

    add mode config & add another photocard mode

--- components/PhotoCard.tsx
+++ components/PhotoCard.tsx
@@ -13,6 +13,8 @@ import { PostContent } from '../utils/PostContent';
 
 import getNotionPostSlug from '../utils/getNotionPostSlug';
 
+import modeConfig from '../config/mode.config';
+
 const PhotoCard = ({ post }: { post: PostContent }) => {
     const { t } = useTranslation('common');
 
@@ -40,34 +42,44 @@ const PhotoCard = ({ post }: { post: PostContent }) => {
                             {post.title}
                         </Link>
                     </div>
-                    <svg className='invisible animate-spin -ml-1 mr-3 h-5 w-5 text-white' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
-                        <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
-                        <path className='opacity-75' fill='currentColor' d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
-                    </svg>
                 </div>
-
-                <div id='notion-photo-body-card-intro' className='flex items-center space-x-6 md:space-x-8'>
-                    <div id='notion-photo-body-card-date' className='flex items-center'>
-                        <span className='flex flex-col'>
-                            <MdDateRange className='mb-1' />
-                            <span className='text-sm'>{new Date(post.date).toLocaleDateString()}</span>
-                        </span>
-                    </div>
-
-                    <div id='notion-photo-body-card-tags' className='flex items-center'>
-                        <span className='flex flex-col'>
-                            <TiLocationArrowOutline className='mb-1' />
-                            <span className='flex flex-row'>
-                                <div
-                                    id='notion-photo-body-card-tags-item'
-                                    className='rounded ms:border-2 px-2 py-1 mb-1 mx-1 text-sm text-indigo-500 dark:text-indigo-500 bg-blue-100 dark:bg-blue-100'
-                                >
-                                    <span className='text-sm'>{post.location}</span>
-                                </div>
+                {modeConfig.home.card.mode === 0 ? (
+                    <div id='notion-photo-body-card-intro' className='flex items-center space-x-6 md:space-x-8'>
+                        <div id='notion-photo-body-card-date' className='flex items-center'>
+                            <span className='flex flex-col'>
+                                <MdDateRange className='mb-1' />
+                                <span className='text-sm dark:text-gray-300'>
+                                    {new Date(post.date).toLocaleDateString(modeConfig.home.card.date.locale, { year: 'numeric', month: 'short', day: 'numeric' })}
+                                </span>
                             </span>
-                        </span>
+                        </div>
+
+                        <div id='notion-photo-body-card-tags' className='flex items-center'>
+                            <span className='flex flex-col'>
+                                <TiLocationArrowOutline className='mb-1' />
+                                <span className='flex flex-row'>
+                                    <div id='notion-photo-body-card-tags-item' className='bg-blue-200 dark:bg-[#728796] px-3 rounded-xl'>
+                                        <span className='text-sm dark:text-gray-300'>{post.location}</span>
+                                    </div>
+                                </span>
+                            </span>
+                        </div>
                     </div>
-                </div>
+                ) : (
+                    <div id='notion-photo-body-card-intro'>
+                        <div className='flex items-center space-x-3'>
+                            <div id='notion-photo-body-card-date'>
+                                <span className='text-sm dark:text-gray-300'>
+                                    {new Date(post.date).toLocaleDateString(modeConfig.home.card.date.locale, { year: 'numeric', month: 'short', day: 'numeric' })}
+                                </span>
+                            </div>
+                            <div>•</div>
+                            <div id='notion-photo-body-card-tags-item' className='bg-blue-200 dark:bg-[#728796] px-3 rounded-xl'>
+                                <span className='text-sm dark:text-gray-300'>{post.location}</span>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
